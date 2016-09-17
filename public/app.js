355916e678c458a109b13529c40d3cefe4bfefdb
Hasnain Khan <HasnainKhankqs@gmail.com>
2016-09-17 11:41:36 -0700

    adding project to firebase

---
+++ public/app.js
@@ -0,0 +1,39 @@
+// How can we assign variable values and alert it.
+var a = "Hellow";
+alert(a);
+// how can get something from prompt and alert it.
+var b = prompt("type your name", "Hasnain");
+alert(b);
+
+// // How can we concatinate two strings.
+var c = prompt("what's Your name: ", "Hasnain");
+var d = prompt("What's Last name: ", "Khan");
+alert(c+" "+d);
+// How can we concatinate two numbers.
+var e = prompt("type any integer number: ", "7");
+var f = prompt("type another integer number: ", "5");
+var g = parseInt(e);
+var h = parseInt(f);
+var i = g+h;
+alert(i);
+
+
+
+// How can we assign variable values and write  it on html file.
+var a = "Hellow";
+document.write(a);
+// how can get something from prompt and alert it.
+var b = prompt("Type your name", "Hasnain");
+document.write(b);
+
+// // How can we concatinate two strings.
+var c = prompt("what's Your name: ", "Hasnain");
+var d = prompt("What's Your Last name: ", "Khan");
+document.write(c+" "+d);
+// How can we concatinate two numbers.
+var e = prompt("type any integer number: ", "7");
+var f = prompt("type another integer number: ", "5");
+var g = parseInt(e);
+var h = parseInt(f);
+var i = g+h;
+document.write(i);
